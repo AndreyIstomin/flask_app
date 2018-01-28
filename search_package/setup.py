360cbb0cdfdece08360bfba7b99574e7579b0e69
Andrey Istomin <istomin_andrey@outlook.com>
2018-01-28 23:31:29 +0300

    Initial commit

---
+++ search_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='search_package',
+    version=1.0,
+    descriptoin='The head first python search tools',
+    author='HF Python 2e',
+    author_email='hfpy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['search_for_letters']
+)
