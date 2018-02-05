a02de6ec5270ee270cccd3325ac5e5ce8b6c31f1
Andrey Istomin <istomin_andrey@outlook.com>
2018-02-05 13:05:27 +0300

    database context manager

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, escape
 from search_for_letters import search4letters
+from DBcm import UseDatabase
 
 app = Flask(__name__)
+
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'flask_app',
+                          'password': '123456',
+                          'database': 'vsearchlogDB',}
 
 
 @app.route('/search4', methods=['POST'])
@@ -26,26 +32,57 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 def view_the_log() -> str:
 
-    contents = []
+    with UseDatabase(app.config['dbconfig']) as cursor:
 
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
 
-    titles = ('Form data', 'Remote_addr', 'User_agent', 'Results')
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html',
                            the_title='View log',
                            the_row_titles=titles,
-                           the_data=contents)
+                           the_data=contents,)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
 
-    with open('vsearch.log', 'a') as log_file:
+    with UseDatabase(app.config['dbconfig']) as cursor:
 
-        print(req.form, req.remote_addr, req.user_agent, res, sep='|', file=log_file)
+        _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+
+# deprecated
+# def log_request_to_file(req: 'flask_request', res: str) -> None:
+#
+#     with open('vsearch.log', 'a') as log_file:
+#
+#         print(req.form, req.remote_addr, req.user_agent, res, sep='|', file=log_file)
+#
+#
+# @app.route('/viewlog_file')
+# def view_the_log_from_file() -> str:
+#
+#     contents = []
+#
+#     with open('vsearch.log') as log:
+#         for line in log:
+#             contents.append([])
+#             for item in line.split('|'):
+#                 contents[-1].append(escape(item))
+#
+#     titles = ('Form data', 'Remote_addr', 'User_agent', 'Results')
+#     return render_template('viewlog.html',
+#                            the_title='View log',
+#                            the_row_titles=titles,
+#                            the_data=contents)
 
 # app.run(host='0.0.0.0')
 if __name__ == '__main__':
